ae5b65051b1225af60020609cd6d4643f55790bd
ariyaaup <ariyautama77@gmail.com>
2025-06-08 19:06:59 +0700

    First Commit

---
+++ app/src/main/java/com/example/convertqu/ConvertQuService.kt
@@ -0,0 +1,8 @@
+package com.example.convertqu
+
+import retrofit2.http.GET
+
+interface ConvertQuService {
+  /*  @GET("fact")
+    suspend fun getRandomFact() : CatFact*/
+}
---
+++ app/src/main/java/com/example/convertqu/HomeActivity.kt
@@ -0,0 +1,4 @@
+package com.example.convertqu
+
+class HomeActivity {
+}
